0f8050bbca3bd590f998e610c73ea0897a109d6c
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-09-20 15:22:25 -0500

    complete the first five exercises for chinook

--- chinook-queries.sql
+++ chinook-queries.sql
@@ -5,3 +5,10 @@ SELECT FirstName, LastName, CustomerId, Country FROM Customer WHERE Country IS N
 SELECT * FROM Customer WHERE Country IS "Brazil"
 
 -- 3. Provide a query showing the Invoices of customers who are from Brazil. The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country.
+SELECT c.FirstName, c.LastName, i.InvoiceId, i.InvoiceDate, i.BillingCountry 
+FROM Customer c LEFT JOIN Invoice i WHERE c.Country IS "Brazil" 
+
+-- 4. Provide a query showing only the Employees who are Sales Agents.
+SELECT * FROM Employee WHERE Title IS "Sales Support Agent"
+
+-- 5. Provide a query showing a unique list of billing countries from the Invoice table.
